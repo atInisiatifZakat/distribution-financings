abc380f8324f697e2a7a7c63c1de4b9649d76f8f
nuradiyana <nuradiyana@users.noreply.github.com>
2024-11-01 10:46:41 +0000

    Fix styling

--- src/Models/Program.php
+++ src/Models/Program.php
@@ -6,7 +6,6 @@ namespace Inisiatif\Distribution\Financings\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Inisiatif\Package\Contract\Common\Model\ResourceInterface;
 
 final class Program extends Model implements ResourceInterface
