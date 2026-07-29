590a479f5f4ba324bb25266e70a7723b4efc6084
Annisa Nadia n <annisanadian@gmail.com>
2026-07-29 11:48:23 +0700

    menambahkan sedikit penyesuaian

--- src/Repositories/DonationProgramRepository.php
+++ src/Repositories/DonationProgramRepository.php
@@ -8,26 +8,17 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
 use Spatie\QueryBuilder\QueryBuilder;
 use Spatie\QueryBuilder\AllowedFilter;
-use Illuminate\Database\Eloquent\Builder;
-use Inisiatif\Package\Common\Abstracts\AbstractRepository;
-use Inisiatif\Distribution\Financings\Models\DonationProgram;
+use Inisiatif\ModelShared\ModelShared;
 
-final class DonationProgramRepository extends AbstractRepository
+final class DonationProgramRepository
 {
-    protected $model = DonationProgram::class;
-
     public function fetchAll(Request $request): Collection
     {
-        $builder = $this->getModel()->newQuery()->orderBy('name');
-
-        return $this->queryBuilder($builder, $request)->get();
-    }
-
-    public function queryBuilder(Builder $builder, Request $request): QueryBuilder
-    {
-        return QueryBuilder::for($builder, $request)->allowedFilters([
-            AllowedFilter::partial('name', 'name'),
-            AllowedFilter::exact('id', 'id'),
-        ]);
+        return QueryBuilder::for(ModelShared::getProgramModel()->newQuery()->orderBy('name'), $request)
+            ->allowedFilters([
+                AllowedFilter::partial('name', 'name'),
+                AllowedFilter::exact('id', 'id'),
+            ])
+            ->get();
     }
 }
